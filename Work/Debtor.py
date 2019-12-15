2570e3ef15eb6e6245d15c248074ac302657f91b
ProfNimnul <stanso@ukr.net>
2019-12-16 01:31:33 +0200

    убрал ошибки

--- Work/Debtor.py
+++ Work/Debtor.py
@@ -2,7 +2,7 @@ import win32com.client as w32
 from os import path as p
 from os import remove
 from easygui import msgbox, fileopenbox
-
+import sys
 
 class Debtor:
     codes = {1: "площа Праці",
@@ -16,7 +16,7 @@ class Debtor:
              17: "Спортивна",
              18: "Парусна",
              19: "Віталія Шума",
-             2: "проспект Миру"
+             2: "проспект Миру",
              20: "Данченко",
              3: "Олександрійська",
              31: "Лазурна",
@@ -57,6 +57,7 @@ class Debtor:
         '\\': '/'
 
     }  # и т. д
+
     ## Блок глобвльных данных
 
     # *****************************************************************
@@ -96,14 +97,12 @@ class Debtor:
 
     # *****************************************************************
 
+    def ReplaceInSheet(self, Excel, wb, ws, dictionary: dict, range: str, invert: bool = False):
 
-
-
-    def ReplaceInSheet(self, Excel, wb, ws, dictionary:dict, invert:bool =  False, range:str):
-
-        def inv_dict(d: dict) -> dict  # инвертирует словарь
+        def inv_dict(d: dict) -> dict:  # инвертирует словарь
             d = {v: k for k, v in d.items()}
             return d
+
         # wb- книга, открытая методом OpenXLS
 
         if invert:
@@ -127,8 +126,6 @@ class Debtor:
 
         # wb.Close ( )
         # Excel.Quit ( )
-
-        return
 
     # *****************************************************************
 
@@ -155,95 +152,95 @@ class Debtor:
     # *****************************************************************
 
     # *****************************************************
-
-    def addHeader(self, ws):
-        import datetime
-        current_month = datetime.datetime.today().month
-
-        month_name = {1: "січня",
-                      2: "лютого",
-                      3: "березня",
-                      4: "квітня",
-                      5: "травня",
-                      6: "червня",
-                      7: "липня",
-                      8: "серпня",
-                      9: "вересня",
-                      10: "жовтня",
-                      11: "листопада",
-                      12: "грудня"
-                      }[current_month]
-
-        header_city = "м. Чорноморськ"
-        header_with_month = "Перелік боржників за послуги з утримання будинків та прибудинкових теріторій" \
-                            "  станом на 01 {} 2017 р. ".format(month_name)
-
-        header_warning = "Усім боржникам потрібно терминово погасити існуючу заборгованість" \
-                         " для подальшого відповідного надання послуг" \
-                         " з утримання будинків та прибудинкових територій!"
-
-        ws.Range("2:9").Insert(-4142)
-        # for _ in range(5):
-        #     # посмотреть и изучить количество аргументов и порядок их передачи!!!
-        #     ws.Selection.Insert (Shift = -4121, CopyOrigin = 0)
-
-        ws.Range("A1:F5").ClearContents()
-
-        # ws.Selection.HorizontalAlignment = -4108 #xlCenter
-        # ws.Selection.VerticalAlignment = -4160 #xlTop
-        # ws.Selection.WrapText = -1
-        # ws.Selection.Orientation = 0
-        # ws.Selection.AddIndent = 0
-        # ws.Selection.IndentLevel = 0
-        # ws.Selection.ShrinkToFit = 0
-        # ws.Selection.ReadingOrder = -5002 #xlContext
-        # ws.Selection.MergeCells = -1
-        # ws.Selection.Font.Size = 20
-        # ws.Selection.Font.Bold = -1
-        # ws.Selection.Font.Color = -16776961
-
-        # ws.Selection.Merge()
-        ws.Range("A1:F1").Merge()
-        ws.Range('A1').HorizontalAlignment = -4108  # xlCenter
-        ws.Range('A1').VerticalAlignment = -4160  # xlTop
-
-        ws.Range("A1").Value = header_city
-        ws.Range('A1').Font.Bold = -1
-
-        ws.Range('A3:F4').Merge()
-        ws.Range('A3').HorizontalAlignment = -4108  # xlCenter
-        ws.Range('A3').VerticalAlignment = -4160  # xlTop
-        ws.Range('A3').ShrinkToFit = 0
-        ws.Range('A3').WrapText = -1
-        ws.Range('A3').Font.Bold = -1
-        ws.Range("A3").Value = header_with_month
-
-        ws.Range('A5:F7').Merge()
-        ws.Range('A5').HorizontalAlignment = -4108  # xlCenter
-        ws.Range('A5').VerticalAlignment = -4160  # xlTop
-        ws.Range('A5').WrapText = -1
-        ws.Range('A5').Orientation = 0
-        ws.Range('A5').AddIndent = 0
-        ws.Range('A5').IndentLevel = 0
-        ws.Range('A5').ShrinkToFit = -1
-        ws.Range('A5').ReadingOrder = -5002  # xlContext
-        ws.Range('A5').MergeCells = -1
-        ws.Range('A5').Font.Size = 16
-        ws.Range('A5').Font.Bold = -1
-        ws.Range('A5').Font.Color = -16776961
-        ws.Range("A5").RowHeight = 58.5
-
-        ws.Range("A5").Value = header_warning
-
-        ws.Range('A10').Font.Bold = -1
-        ws.Range('B10').Font.Bold = -1
-        ws.Range('C10').Font.Bold = -1
-
-        ws.Range('A10').Value = "Вул., буд."
-        ws.Range('B10').Value = "кв."
-        ws.Range('C10').Value = "Борг"
-        ## Все константі перевести в числовой вид!!!
-    # *****************************************************************
+    #
+    # def addHeader(self, ws):
+    #     import datetime
+    #     current_month = datetime.datetime.today().month
+    #
+    #     month_name = {1: "січня",
+    #                   2: "лютого",
+    #                   3: "березня",
+    #                   4: "квітня",
+    #                   5: "травня",
+    #                   6: "червня",
+    #                   7: "липня",
+    #                   8: "серпня",
+    #                   9: "вересня",
+    #                   10: "жовтня",
+    #                   11: "листопада",
+    #                   12: "грудня"
+    #                   }[current_month]
+    #
+    #     header_city = "м. Чорноморськ"
+    #     header_with_month = "Перелік боржників за послуги з утримання будинків та прибудинкових теріторій" \
+    #                         "  станом на 01 {} 2017 р. ".format(month_name)
+    #
+    #     header_warning = "Усім боржникам потрібно терминово погасити існуючу заборгованість" \
+    #                      " для подальшого відповідного надання послуг" \
+    #                      " з утримання будинків та прибудинкових територій!"
+    #
+    #     ws.Range("2:9").Insert(-4142)
+    #     # for _ in range(5):
+    #     #     # посмотреть и изучить количество аргументов и порядок их передачи!!!
+    #     #     ws.Selection.Insert (Shift = -4121, CopyOrigin = 0)
+    #
+    #     ws.Range("A1:F5").ClearContents()
+    #
+    #     # ws.Selection.HorizontalAlignment = -4108 #xlCenter
+    #     # ws.Selection.VerticalAlignment = -4160 #xlTop
+    #     # ws.Selection.WrapText = -1
+    #     # ws.Selection.Orientation = 0
+    #     # ws.Selection.AddIndent = 0
+    #     # ws.Selection.IndentLevel = 0
+    #     # ws.Selection.ShrinkToFit = 0
+    #     # ws.Selection.ReadingOrder = -5002 #xlContext
+    #     # ws.Selection.MergeCells = -1
+    #     # ws.Selection.Font.Size = 20
+    #     # ws.Selection.Font.Bold = -1
+    #     # ws.Selection.Font.Color = -16776961
+    #
+    #     # ws.Selection.Merge()
+    #     ws.Range("A1:F1").Merge()
+    #     ws.Range('A1').HorizontalAlignment = -4108  # xlCenter
+    #     ws.Range('A1').VerticalAlignment = -4160  # xlTop
+    #
+    #     ws.Range("A1").Value = header_city
+    #     ws.Range('A1').Font.Bold = -1
+    #
+    #     ws.Range('A3:F4').Merge()
+    #     ws.Range('A3').HorizontalAlignment = -4108  # xlCenter
+    #     ws.Range('A3').VerticalAlignment = -4160  # xlTop
+    #     ws.Range('A3').ShrinkToFit = 0
+    #     ws.Range('A3').WrapText = -1
+    #     ws.Range('A3').Font.Bold = -1
+    #     ws.Range("A3").Value = header_with_month
+    #
+    #     ws.Range('A5:F7').Merge()
+    #     ws.Range('A5').HorizontalAlignment = -4108  # xlCenter
+    #     ws.Range('A5').VerticalAlignment = -4160  # xlTop
+    #     ws.Range('A5').WrapText = -1
+    #     ws.Range('A5').Orientation = 0
+    #     ws.Range('A5').AddIndent = 0
+    #     ws.Range('A5').IndentLevel = 0
+    #     ws.Range('A5').ShrinkToFit = -1
+    #     ws.Range('A5').ReadingOrder = -5002  # xlContext
+    #     ws.Range('A5').MergeCells = -1
+    #     ws.Range('A5').Font.Size = 16
+    #     ws.Range('A5').Font.Bold = -1
+    #     ws.Range('A5').Font.Color = -16776961
+    #     ws.Range("A5").RowHeight = 58.5
+    #
+    #     ws.Range("A5").Value = header_warning
+    #
+    #     ws.Range('A10').Font.Bold = -1
+    #     ws.Range('B10').Font.Bold = -1
+    #     ws.Range('C10').Font.Bold = -1
+    #
+    #     ws.Range('A10').Value = "Вул., буд."
+    #     ws.Range('B10').Value = "кв."
+    #     ws.Range('C10').Value = "Борг"
+    #     ## Все константі перевести в числовой вид!!!
+    # # *****************************************************************
 
 
 if __name__ == '__main__':
@@ -253,7 +250,7 @@ if __name__ == '__main__':
 
     Excel, wb, ws = debtor.openExcelInstance(fname)
 
-    debtor.addHeader(ws)
+  #  debtor.addHeader(ws)
     debtor.ReplaceInSheet(Excel, wb, ws)
 
     path = p.dirname(fname)
